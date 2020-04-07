7bfa1da824f4e032775ed1d594f373035d064b4e
Tanvir Rahman <ornobtanvir.git@gmail.com>
2020-04-07 11:28:43 +0600

    added the route

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -3,17 +3,42 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+
+## First we make the get request
+## it will return all the 
+## meeting information
+## we also deffine which option is possible
+## and with except key word we will show 
+## which they cant
+## this route can't do the update and post
+## only get
+Route::resource('/meeting','MeetingController',[
+    'except' => ['edit','create']
+]);
+
+
+
+## add another route
+## can only do the post and delete
+
+Route::resource('meeting/registration','RegistrationController',[
+    'only' => ['store','destroy']
+]);
+
+## add route for users only the post nothing else
+## we can define the controller using 'uses'
+## this is the default uses of the post
+
+## this is the registration post
+Route::post('user',[
+    'uses' => 'AuthController@store'
+]);
+
+
+## now the sign in endpoint
+
+Route::post('user/signin',[
+    'uses' => ''
+]);
