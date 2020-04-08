dab5b1ddccbbdb6c829db12c81b2b267f42c12ec
Tanvir Rahman <ornobtanvir.git@gmail.com>
2020-04-08 15:35:27 +0600

    added

--- laravel/app/Http/Controllers/RegistrationController.php
+++ laravel/app/Http/Controllers/RegistrationController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Meeting;
+use App\User;
 use Illuminate\Http\Request;
 
 class RegistrationController extends Controller
@@ -37,7 +39,64 @@ class RegistrationController extends Controller
         ## this will make meeting 
         ## registration
         ## not user registration
-        ## it take 
+        ## it take
+
+        
+        ## first validate
+        $this->validate($request,[
+            // this will validate the meeting and user_id
+            'meeting_id' => 'required',
+            'user_id' => 'required'
+        ]);
+
+        // now get the meeting the id and the user_id
+
+        $meeting_id  = $request->input('meeting_id');
+        $user_id  = $request->input('user_id');
+
+        // now find if the meeting exists or 
+        // the meeting exists
+        // find the meeting and the user with the id
+        $meeting = Meeting::findOrFail($meeting_id);
+        $user = User::findOrFail($user_id);
+
+
+        ### create the message
+
+        $message = [
+            'msg' => "User is already registered",
+            'user' => $user,
+            'meeting' => $meeting,
+            'unregister' => [
+                'href' => 'api/meeting/registration/' .$meeting->id,
+                'method' => 'DELETE'
+            ]
+        ];
+
+        if($meeting->users()->where('users.id',$user->id)->first()){
+            // if you find the user in the join query
+            return response()->json($message,404);
+        }
+
+        // if not attach to the meeting
+        // you can do it with reverse order too
+        $user->meetings()->attach($meeting);
+
+
+        // create the response
+
+        $message = [
+            'msg' => "User is registered for the meeting",
+            'user' => $user,
+            'meeting' => $meeting,
+            'unregister' => [
+                'href' => 'api/meeting/registration/' .$meeting->id,
+                'method' => 'DELETE'
+            ]
+        ];
+
+        return response()->json($message,201);
+
 
     }
 
@@ -83,6 +142,15 @@ class RegistrationController extends Controller
      */
     public function destroy($id)
     {
-        return "it worked";
+        $meeting = Meeting::findOrFail($id);
+        // do not delete the meeting just 
+        // delete the user from the meeting
+        $meeting->users()->detach();
+        $response = [
+            'msg' => "User unregistered from the meeting",
+            'meeting' => $meeting,
+        ];
+
+        return response()->json($response,200);
     }
 }
